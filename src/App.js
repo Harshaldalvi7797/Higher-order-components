64812477239d26b8c49dc488e04bdbaac94886a2
Harshaldalvi7797 <[dalviharshal7797@gmail.com]>
2020-03-15 02:39:52 +0530

    commit : post data fetch

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Post from "./components/post/post";
 
 import "./App.css";
 
@@ -6,7 +7,12 @@ class App extends Component {
   render() {
     return (
       <div className="container">
-        <h1>Welcome</h1>
+        <div className="row">
+          <div className="col-md-6">
+            <Post />
+          </div>
+          <div className="col-md-6"></div>
+        </div>
       </div>
     );
   }
